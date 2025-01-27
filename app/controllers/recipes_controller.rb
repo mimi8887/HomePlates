ca6a24c6121f26b51473c78cd2b555a7c4ffe6eb
Simone Mine <simonminekoza@gmail.com>
2025-01-27 19:40:55 +0100

    showpage connected

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,12 +10,16 @@ class RecipesController < ApplicationController
   end
 
   def show
-  recipe_id = params[:id]
-  url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1045425526601869289"
-  recipes_serialized = URI.parse(url).read
-  recipes = JSON.parse(recipes_serialized)
-  @recipe = recipes["result"].find{|recipe| recipe["recipeId"] == recipe_id}
-  
+    if params[:recipe].present?
+      @recipe = JSON.parse(params[:recipe])
+    else
+      recipe_id = params[:id]
+      url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1045425526601869289"
+      recipes_serialized = URI.parse(url).read
+      recipes = JSON.parse(recipes_serialized)
+      @recipe = recipes["result"].find { |recipe| recipe["recipeId"].to_s == recipe_id }
+    # raise
+    end
   end
 
   def category
